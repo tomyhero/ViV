b3ad735472bbb862a7e9bd8fd87632628adde4fb
tomyhero <tomyhero@cpan.org>
2010-04-27 16:06:43 +0900

    apply i18n for member index / add

--- lib/ViV/Web/View.pm
+++ lib/ViV/Web/View.pm
@@ -1,4 +1,13 @@
 package ViV::Web::View;
 use Polocky::Class;
+use ViV::Container 'con';
 extends qw(Polocky::WAF::View);
+
+
+sub _build_stash {
+    my ( $self, $c, $type ) = @_;
+    $c->stash->{c} = $c;
+    $c->stash->{config} = $c->config;
+    $c->stash->{i18n} = con('i18n');
+}
 __POLOCKY__;
--- resource/i18n/ja.pl
+++ resource/i18n/ja.pl
@@ -1,9 +1,24 @@
 use utf8;
 {
     label => {
-        password => 'パスワード',
         login_name => 'ログイン名',
-        on_admin => '管理権限あり',
+        password => 'パスワード',
+        password_confirm => 'パスワード確認',
+        on_admin => '管理権限',
         screen_name => '表示名',
+    },
+    common => {
+        on   => 'あり',
+        off  => 'なし',
+    },
+    navi => {
+        'add' => '追加',
+        'edit' => '編集',
+        'delete' => '削除',
+    },
+    form => {
+        'add' => '追加する',
+        'edit' => '編集する',
+        'delete' => '削除する',
     },
 }
